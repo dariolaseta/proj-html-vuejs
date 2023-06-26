c93b5342adc7f004be5a4a876930c428f346869f
Oxidra <dantelezzo@gmail.com>
2023-06-26 12:22:30 +0200

    started header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,27 @@
 <template>
     <header>
-        <div class="logo">
-            Logo
-        </div>
-
-        <div class="middle-section">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="@">Services</a></li>
-                <li><a href="#">Why Us</a></li>
-                <li><a href="#">Case Studies</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Blog</a></li>
-            </ul>
-        </div>
-
-        <div class="right-section">
-            <ul>
-                <li><a href="#">(555) 802-1234</a></li>
-                <li><a href="#"><button>Free Quote</button></a></li>
-            </ul>
+        <div class="container">
+            <div class="logo">
+                Logo
+            </div>
+    
+            <div class="middle-section">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="@">Services</a></li>
+                    <li><a href="#">Why Us</a></li>
+                    <li><a href="#">Case Studies</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+    
+            <div class="right-section">
+                <ul>
+                    <li><a href="#">(555) 802-1234</a></li>
+                    <li><a href="#"><button>Free Quote</button></a></li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
@@ -35,6 +37,31 @@ export default {
     @use "../styles/partials/_variables" as *;
 
     header{
-        
+        background-color: red;
+        div.container{
+            padding: 1.5rem 0;
+            @include flex(row, space-between, center);
+
+            div.logo{
+            }
+
+            div.middle-section{
+                width: 60%;
+                ul{
+                    @include flex(row, space-between, center);
+                }
+            }
+
+            div.right-section{
+                ul{
+                    @include flex(row, normal, normal);
+
+                    li{
+                        margin-right: 1rem;
+                    }
+                }
+            }
+        }
+
     }
 </style>
