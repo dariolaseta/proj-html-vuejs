52dda77858f6d42c8c80a64695ac8f67094335c8
Oxidra <dantelezzo@gmail.com>
2023-06-26 13:24:58 +0200

    added svg & started creation center section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,22 @@
 <template>
     <AppJumbo />
+    <MainCenterApp />
 </template>
 
 <script>
 import AppJumbo from "./AppJumbo.vue"
+import MainCenterApp from "./MainCenterApp.vue"
 
 export default {
     name: "AppMain",
     components:{
-        AppJumbo
+        AppJumbo,
+        MainCenterApp
     } 
 }
 </script>
 
 <style scoped lang="scss">
-    
+    @use "../styles/partials/_mixins" as *;
+    @use "../styles/partials/_variables" as *;
 </style>
---
+++ src/components/MainCenterApp.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="increase-brand">
+        <div class="container">
+            <div class="image-part">
+                <img src="" alt="">
+            </div>
+
+            <div class="text-part">
+                
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "MainCenterApp"
+}
+</script>
+
+<style scoped lang="scss">
+    @use "../styles/partials/_mixins" as *;
+    @use "../styles/partials/_variables" as *;
+
+    section.increase-brand{
+        div.container{
+            background-color: purple;
+            height: 500px;
+            width: 100%;
+        }
+    }
+</style>
